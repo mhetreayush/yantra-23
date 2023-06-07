30e8d4e787a07e621cfa83076c814ea571caa2d7
Ayush <mhetreayush1719@gmail.com>
2023-06-07 15:23:14 +0530

    🔍🔍 Integrate search

--- src/pages/trash/index.jsx
+++ src/pages/trash/index.jsx
@@ -1,33 +1,47 @@
 import TrashCard from "@/component/TrashCard";
 import Link from "next/link";
 import trashMap from "@/../fakerData.json";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
+import FuzzySearch from "fuzzy-search";
 const Trash = () => {
-  const genData = () => {
-    let data = [];
-    for (let i = 0; i < 5; i++) {
-      data.push({
-        name: faker.name.firstName(),
-        id: faker.datatype.uuid(),
-        price: faker.datatype.number(),
-        seller: faker.name.lastName(),
-        byProducts: [
-          faker.random.word(),
-          faker.random.word(),
-          faker.random.word(),
-        ],
-      });
-    }
-    return data;
-    // return "asd";
-  };
+  const [result, setResult] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  //   const genData = () => {
+  //     let data = [];
+  //     for (let i = 0; i < 5; i++) {
+  //       data.push({
+  //         name: faker.name.firstName(),
+  //         id: faker.datatype.uuid(),
+  //         price: faker.datatype.number(),
+  //         seller: faker.name.lastName(),
+  //         byProducts: [
+  //           faker.random.word(),
+  //           faker.random.word(),
+  //           faker.random.word(),
+  //         ],
+  //       });
+  //     }
+  //     return data;
+  //     // return "asd";
+  //   };
+  const searcher = new FuzzySearch(trashMap, ["name", "byProducts"], {
+    caseSensitive: false,
+    sort: true,
+  });
   useEffect(() => {
-    console.log(genData());
-  }, []);
+    setResult(searcher.search(searchTerm));
+  }, [searchTerm]);
+
   return (
     <>
-      {trashMap.map((trash, idx) => {
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-full p-2 rounded-md border-2 border-black/50"
+      />
+      {result.map((trash, idx) => {
         return (
           <Link href={`trash/${trash.id}`} key={idx}>
             <TrashCard {...trash} />
